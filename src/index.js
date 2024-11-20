4743d35503b71c61763da1c3755fcb2879eb3d61
gemmasmedley90 <gemmasmedley90@gmail.com>
2024-11-20 19:54:59 +0000

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
 function displayPoem(response) {
-    new Typewriter("#poem", {
-     strings: response.data.answer,
-     autoStart: true,
-     delay: 1,
-     cursor: "",
-   });
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 function generatePoem(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let instructionsInput = document.querySelector("#user-instructions");
-    let apiKey = "a00b270af95f4133b29a428e3dote6b0";
-    
-    let context = "You are a seasonal poem expert and love to write short poems. Your mission is to generate a 4 line poem and seperate each line with a <br />. Make sure to follow the user instructions. Sign the poem with <strong>'SheCodes AI'</strong>";   
-    let prompt = `User instuctions: Generate a christmas poem about ${instructionsInput.value}`;
-    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a00b270af95f4133b29a428e3dote6b0";
 
-    axios.get(apiURL).then(displayPoem);
+  let context =
+    "You are a seasonal poem expert and love to write short poems. Your mission is to generate a 4 line poem and seperate each line with a <br />. Make sure to follow the user instructions. Sign the poem with <strong>'SheCodes AI'</strong>";
+  let prompt = `User instuctions: Generate a christmas poem about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⌛ Generating a christmas poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
-let poemFormElement= document.querySelector("#poem-generator-form")
+let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
